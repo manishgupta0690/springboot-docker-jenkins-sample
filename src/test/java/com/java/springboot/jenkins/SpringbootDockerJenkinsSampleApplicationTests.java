13a28c96e4bbde3c48bcd648aa416e79b229e620
manishgupta0690 <manishgupta0690@gmail.com>
2021-11-22 15:27:29 +0000

    first commit

---
+++ src/test/java/com/java/springboot/jenkins/SpringbootDockerJenkinsSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.springboot.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerJenkinsSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
